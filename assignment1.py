87e57717f2c290f9e3598b6f12d51ca855ce2af0
Malay3006 <malaykoshti2006@gmail.com>
2025-07-13 11:07:29 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,19 @@
+'''Task1 --> Basic operations '''
+
+a=int(input("enter your 1st value: "))
+b=int(input("enter your second value: "))
+sum=a+b
+sub=a - b
+mul=a*b
+div=a/b
+print("sum is =",sum)
+print("substraction is = ",sub)
+print("Multiplication is = ",mul)
+print("division 1 is = ",div)
+
+
+#Task 2 greetings
+
+fname=(input("enter FIRST NAME "))
+lname=(input("enter your last name "))
+print("hello",fname,lname +"! welcome to the python program")
